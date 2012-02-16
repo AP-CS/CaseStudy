ec7102a8130bd8747413ecc871b9030a2a3f9324
Zenexer <techsoftadvanced@gmail.com>
2012-02-16 02:45:40 -0500

    Added private constructor to prevent TestScoresApp from being instantiated, as it is a static class.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

--- CaseStudy/src/com/earth2me/school/casestudy/TestScoresApp.java
+++ CaseStudy/src/com/earth2me/school/casestudy/TestScoresApp.java
@@ -2,6 +2,16 @@ package com.earth2me.school.casestudy;
 
 public final class TestScoresApp
 {
+	/**
+	 * Prevents the class from being instantiated.  Do not use.  Do not remove.
+	 * @deprecated
+	 */
+	@Deprecated
+	private TestScoresApp()
+	{
+		// Intentionally blank.
+	}
+	
 	public static void main(String[] args)
 	{
 		TestScoresModel model = new TestScoresModel();
